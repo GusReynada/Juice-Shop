ab0b14f0b72650596e190fadd4ae3c0887b38527
Gustavo Reynada <gustavoreynada12@gmail.com>
2026-07-21 20:59:46 -0600

    Codigo Arreglado

--- routes/appVersion.ts
+++ routes/appVersion.ts
@@ -8,14 +8,9 @@ import { type Request, type Response } from 'express'
 
 import * as utils from '../lib/utils'
 
-export function retrieveAppVersion () {
+export function retrieveAppVersion() {
   return (_req: Request, res: Response) => {
-    if (_req.query.input) {
-      // Vulnerabilidad intencional: eval de entrada de usuario sin sanitizar
-      // CodeQL detectará esto como una vulnerabilidad crítica de inyección de código (js/code-injection)
-      // eslint-disable-next-line no-eval
-      eval(String(_req.query.input))
-    }
+
     res.json({
       version: config.get('application.showVersionNumber') ? utils.version() : ''
     })
